1d3024feb17b1edcee5ef29139810da88768c9eb
Google AI Edge <ai-edge-bot@google.com>
2026-07-10 09:56:36 -0700

    Update checksums for LiteRT-LM v0.14.0 binary targets.
    LiteRT-LM-PiperOrigin-RevId: 945762165

--- Package.swift
+++ Package.swift
@@ -32,13 +32,13 @@ let package = Package(
     .binaryTarget(
       name: "CLiteRTLM",
       url: "https://github.com/google-ai-edge/LiteRT-LM/releases/download/v0.14.0/CLiteRTLM.xcframework.zip",
-      checksum: "4a4bdb0e89689ceacc54c2fb7ae0efe8f5dad2404110976a29c3bf6b374a511e"
+      checksum: "dddac2f6713ed65eaf01c18e115d9fec22184adf575cc7856a21387e8ba937e1"
     ),
     // The Prebuilt Binary Target for Mac
     .binaryTarget(
       name: "CLiteRTLM_mac",
       url: "https://github.com/google-ai-edge/LiteRT-LM/releases/download/v0.14.0/CLiteRTLM_mac.xcframework.zip",
-      checksum: "13e818c9d3987afa87f0716884ebf0b6b10677b480717b8b098146e6b4f45847"
+      checksum: "450615483509aaa6d34b321fdc6862e41a224b674468ab10aff64ebe113d21b7"
     ),
     // The Swift Wrapper Target
     .target(
